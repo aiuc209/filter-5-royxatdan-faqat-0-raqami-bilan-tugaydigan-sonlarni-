13cff724880ff31aa8c16499f3de0cb7fdf5317e
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 23:00:25 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Filter 5 Royxatdan Faqat 0 Raqami Bilan Tugaydigan Sonlarni 
+Topic: FILTER – 5: �� Ro‘yxatdan faqat 0 raqami bilan tugaydigan sonlarni ajratib oling.
+"""
+
+def main():
+    print("Welcome to Filter 5 Royxatdan Faqat 0 Raqami Bilan Tugaydigan Sonlarni !")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
